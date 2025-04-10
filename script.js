436096000da71d998669ef3c7809c6f408c30516
Paradoxich <75419034+Paradoxich@users.noreply.github.com>
2025-04-10 16:27:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const carousel = document.querySelector('.carousel');
+let isDragging = false;
+let startPos = 0;
+let currentScroll = 0;
+
+carousel.addEventListener('mousedown', (e) => {
+  isDragging = true;
+  startPos = e.pageX;
+  currentScroll = carousel.scrollLeft;
+});
+
+window.addEventListener('mouseup', () => {
+  isDragging = false;
+});
+
+window.addEventListener('mousemove', (e) => {
+  if (!isDragging) return;
+  e.preventDefault();
+  carousel.scrollLeft = currentScroll + (startPos - e.pageX);
+});
